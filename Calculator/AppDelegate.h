630146fdcc11c01ebfbaa52298642731b77ffe88
Wonka <akhlaq87@gmail.com>
2013-11-02 13:29:37 +0100

    Initial Commit

---
+++ Calculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by Akhlaq Malik on 02.11.13.
+//  Copyright (c) 2013 Akhlaq Malik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Calculator/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Calculator
+//
+//  Created by Akhlaq Malik on 02.11.13.
+//  Copyright (c) 2013 Akhlaq Malik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
